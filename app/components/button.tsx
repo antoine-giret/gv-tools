c15dfa26fea10aa940931b6a0785cc7ea7f73a2b
Antoine Giret <ant.giret@gmail.com>
2026-01-18 10:57:57 +0100

    style: fix design issues

--- app/components/button.tsx
+++ app/components/button.tsx
@@ -15,8 +15,8 @@ export function Button(
   } & ({ href: string } | { onClick: () => void } | { type: 'submit' })
 ) {
   const className =
-    `rounded-md flex items-center justify-center gap-3 px-6 h-9 ${disabled ? `${variant === 'text' ? '' : 'bg-gray-800'} text-gray-600 pointer-events-none` : `${variant === 'text' ? 'hover:bg-black/10 hover:dark:bg-white/10 focus:bg-black/20 focus:dark:bg-white/20' : 'bg-emerald-500 hover:bg-emerald-600 hover:dark:bg-emerald-400'} text-white`} font-small font-bold cursor-pointer`;
-  const iconClassName = `size-4 ${disabled ? 'text-gray-600' : 'text-white'}`;
+    `rounded-md flex items-center justify-center gap-3 px-6 h-9 ${disabled ? `${variant === 'text' ? '' : 'bg-gray-800'} text-black/50 dark:text-white/50 pointer-events-none` : `${variant === 'text' ? 'hover:bg-black/10 hover:dark:bg-white/10 focus:bg-black/20 focus:dark:bg-white/20' : 'bg-emerald-500 hover:bg-emerald-600 hover:dark:bg-emerald-400'} text-white`} font-small font-bold cursor-pointer`;
+  const iconClassName = `size-4 ${disabled ? 'text-black/50 dark:text-white/50' : 'text-white'}`;
 
   if ('href' in props) {
     const { href } = props;
--- app/components/icon-button.tsx
+++ app/components/icon-button.tsx
@@ -13,8 +13,8 @@ export function IconButton(
   } & ({ href: string } | { onClick: () => void })
 ) {
   const className =
-    `rounded-full flex items-center justify-center h-9 w-9 ${disabled ? `text-gray-600 pointer-events-none` : `hover:bg-black/10 hover:dark:bg-white/10 focus:bg-black/20 focus:dark:bg-white/20`} cursor-pointer`;
-  const iconClassName = `size-4 ${disabled ? 'text-gray-400 dark:text-gray-600' : 'text-black dark:text-white'}`;
+    `rounded-full flex items-center justify-center h-9 w-9 ${disabled ? `text-black/50 dark:text-white/50 pointer-events-none` : `hover:bg-black/10 hover:dark:bg-white/10 focus:bg-black/20 focus:dark:bg-white/20`} cursor-pointer`;
+  const iconClassName = `size-4 ${disabled ? 'text-gray-400 dark:text-black/50 dark:text-white/50' : 'text-black dark:text-white'}`;
   const ariaProps = { 'aria-label': label, title: label };
 
   if ('href' in props) {
